9deefd23b802bccbf75077bb898d4eb0040a2634
pya-py <p4ya.py@gmail.com>
2021-09-08 20:04:46 +0430

    an early demo for T3D Online

--- src/App.js
+++ src/App.js
@@ -16,13 +16,14 @@ class App extends Component {
         return (
             <MainLayout>
                 <Switch>
-                    <Route path="t3d/signUp" exact component={SignUp} />
-                    <Route path="t3d/gameDeck" exact component={GameDeck} />
-                    <Route path="t3d/ranking" exact component={Ranking} />
-                    <Route path="t3d/gameRules" exact component={GameRules} />
-                    <Route path="t3d/contactInfo" exact component={ContactInfo} />
+                    <Route path="t3d/signUp" component={SignUp} />
+                    <Route path="t3d/gameDeck" component={GameDeck} />
+                    <Route path="t3d/ranking" component={Ranking} />
+                    <Route path="t3d/gameRules" component={GameRules} />
+                    <Route path="t3d/contactInfo" component={ContactInfo} />
+                    <Route path="t3d/controlPanel" component={ControlPanel} />
                     <Route path="t3d/" exact component={ScoresTable} />
-                    <Route path="t3d/controlPanel" exact component={ControlPanel} />
+                    
                 </Switch>
             </MainLayout>
         );
