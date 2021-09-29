a29d11f6c13f7b27f9da85444561ed6abb8893fb
pya-py <p4ya.py@gmail.com>
2021-09-29 04:51:55 +0330

    T3D Online Frontend by pya-py

--- src/games/GameChatRoom.jsx
+++ src/games/GameChatRoom.jsx
@@ -1,10 +1,4 @@
-import {
-    Alert,
-    Button,
-    Form,
-    Row,
-    InputGroup,
-} from "react-bootstrap";
+import { Alert, Button, Form, Row, InputGroup } from "react-bootstrap";
 import { Fragment, useState } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
@@ -16,39 +10,41 @@ const GameChatRoom = ({ friendID }) => {
     const dispatch = useDispatch();
     const me = useSelector((state) => state.player);
 
-    const composeMessage = () => {
-        console.log(friendID);
+    const composeMessage = (event) => {
+        event.preventDefault();
         dispatch(SendMessageTo(me.fullname, friendID, myMessage));
     };
 
     return (
         <Fragment>
-            <Row className="w-100 mx-auto">
+            <Row className="w-100 mt-3 mx-auto">
                 <Alert className="w-100 text-right" variant="info">
                     {message.recieved ? message.recieved.text : null}
                 </Alert>
             </Row>
             <Row className="w-100 mx-auto mb-3">
-                <InputGroup className="w-100">
-                    <InputGroup.Prepend style={{width:'10%'}}>
-                        <Button
-                            onClick={composeMessage}
-                            className="mx-auto"
-                            variant="outline-info">
-                            <i
-                                className="fa fa-paper-plane"
-                                aria-hidden="true"></i>
-                        </Button>
-                    </InputGroup.Prepend>
+                <Form onSubmit={event => composeMessage(event)}>
+                    <InputGroup className="w-100">
+                        <InputGroup.Prepend style={{ width: "10%" }}>
+                            <Button
+                                type="submit"
+                                className="mx-auto"
+                                variant="outline-info">
+                                <i
+                                    className="fa fa-paper-plane"
+                                    aria-hidden="true"></i>
+                            </Button>
+                        </InputGroup.Prepend>
 
-                    <InputGroup.Prepend style={{width:'90%'}}>
-                        <Form.Control
-                            placeholder="پیام..."
-                            value={myMessage}
-                            onChange={(e) => setMyMessage(e.target.value)}
-                            className="text-right"></Form.Control>
-                    </InputGroup.Prepend>
-                </InputGroup>
+                        <InputGroup.Prepend style={{ width: "90%" }}>
+                            <Form.Control
+                                placeholder="پیام..."
+                                value={myMessage}
+                                onChange={(e) => setMyMessage(e.target.value)}
+                                className="text-right"></Form.Control>
+                        </InputGroup.Prepend>
+                    </InputGroup>
+                </Form>
             </Row>
         </Fragment>
     );
