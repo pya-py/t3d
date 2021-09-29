c4ca0065f3edde36671549d766824d0318c9e045
pya-py <p4ya.py@gmail.com>
2021-09-29 05:05:40 +0330

    T3D Online Frontend by pya-py

--- src/games/GameChatRoom.jsx
+++ src/games/GameChatRoom.jsx
@@ -16,34 +16,33 @@ const GameChatRoom = ({ friendID }) => {
     };
 
     return (
-        <Form onSubmit={event => composeMessage(event)}>
+        <Form onSubmit={(event) => composeMessage(event)}>
             <Row className="w-100 mt-3 mx-auto">
                 <Alert className="w-100 text-right" variant="info">
                     {message.recieved ? message.recieved.text : null}
                 </Alert>
             </Row>
             <Row className="w-100 mx-auto mb-3">
-                
-                    <InputGroup className="w-100">
-                        <InputGroup.Prepend style={{ width: "10%" }}>
-                            <Button
-                                type="submit"
-                                className="mx-auto"
-                                variant="outline-info">
-                                <i
-                                    className="fa fa-paper-plane"
-                                    aria-hidden="true"></i>
-                            </Button>
-                        </InputGroup.Prepend>
+                <InputGroup className="w-100">
+                    <InputGroup.Prepend style={{ width: "10%" }}>
+                        <Button
+                            type="submit"
+                            className="mx-auto"
+                            variant="outline-info">
+                            <i
+                                className="fa fa-paper-plane"
+                                aria-hidden="true"></i>
+                        </Button>
+                    </InputGroup.Prepend>
 
-                        <InputGroup.Prepend style={{ width: "90%" }}>
-                            <Form.Control
-                                placeholder="پیام..."
-                                value={myMessage}
-                                onChange={(e) => setMyMessage(e.target.value)}
-                                className="text-right"></Form.Control>
-                        </InputGroup.Prepend>
-                    </InputGroup>
+                    <InputGroup.Prepend style={{ width: "90%" }}>
+                        <Form.Control
+                            placeholder="پیام..."
+                            value={myMessage}
+                            onChange={(e) => setMyMessage(e.target.value)}
+                            className="text-right"></Form.Control>
+                    </InputGroup.Prepend>
+                </InputGroup>
             </Row>
         </Form>
     );
