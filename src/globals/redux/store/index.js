ce42cc347a2d459718a50d38e6e57c8daaca7883
pya-py <p4ya.py@gmail.com>
2021-10-09 02:16:42 +0330

    redux middleware fix

--- src/globals/redux/store/index.js
+++ src/globals/redux/store/index.js
@@ -3,14 +3,8 @@ import thunk from "redux-thunk";
 import { reducers } from "../reducers";
 
 export const store = createStore(
-	reducers,
-	compose(
-		applyMiddleware(thunk),
-		window.__REDUX_DEVTOOLS_EXTENSION__ &&
-			window.__REDUX_DEVTOOLS_EXTENSION__()//temp: enable chrome redux manager extension
-	)
-	
-);
+    reducers,
+    applyMiddleware(thunk));
 
 //dispatch
 // store.subscribe(() => console.log(store.getState()));
