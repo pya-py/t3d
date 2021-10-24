857f28006e4d9c9ff71167eebb5c98d004dc4563
pya-py <p4ya.py@gmail.com>
2021-10-24 05:52:09 +0330

    .

--- src/globals/redux/store/index.js
+++ src/globals/redux/store/index.js
@@ -1,16 +1,8 @@
-import { applyMiddleware, compose, createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import { reducers } from "../reducers";
 
-export const store = createStore(
-	reducers,
-	compose(
-		applyMiddleware(thunk),
-		window.__REDUX_DEVTOOLS_EXTENSION__ &&
-			window.__REDUX_DEVTOOLS_EXTENSION__()//temp: enable chrome redux manager extension
-	)
-	
-);
+export const store = createStore(reducers, applyMiddleware(thunk));
 
 //dispatch
 // store.subscribe(() => console.log(store.getState()));
